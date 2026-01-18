992abc2f4687d775c68f904dd6b2aae1d5066085
frostdead43 <frostdead@hotmail.com>
2026-01-18 19:35:47 +0300

    Next, a rewrite was written to make the container the gateway in the next config, and the other page structures were changed again

--- container/next.config.mjs
+++ container/next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return[
+      {
+        source: "/products/:path*",
+        destination: "http://localhost:3001/products/:path*",
+      },
+
+         {
+        source: "/cart/:path*",
+        destination: "http://localhost:3002/cart/:path*",
+      },
+
+    ]
+  }
 };
 
 export default nextConfig;
--- microfrontend-store-cart/app/Cart/page.js
+++ microfrontend-store-cart/app/Cart/page.js
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Cart() {
   return (
     <div>Cart1</div>
   );
