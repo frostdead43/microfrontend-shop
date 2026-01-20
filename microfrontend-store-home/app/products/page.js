260fc427d179b4b9aa4a34c50ee5d5c09a2ff898
frostdead43 <frostdead@hotmail.com>
2026-01-20 18:00:40 +0300

    The visual design for the home page has been completed

--- microfrontend-store-home/app/products/page.js
+++ microfrontend-store-home/app/products/page.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default async function Home() {
   const products = await fetchProducts();
   return (
-  <div>
+  <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 mb mx-16 my-20">
     {products.map(x => (
       <Link key={x.id} href={`/products/${x.id}`}>
         <CardItem  product={x}/>
--- microfrontend-store-home/components/ProductCard.jsx
+++ microfrontend-store-home/components/ProductCard.jsx
@@ -1,10 +1,16 @@
 export default function CardItem({ product }) {
   return (
-    <div>
-      <img src={product.image}/>
-      <h3>{product.title}</h3>
-      <h6>{product.price}</h6>
-      <button>Details</button>
+    <div className="w-120 bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
+      <div className="aspect-square w-full overflow-hidden bg-gray-100">
+        <img src={product.image} className="w-full h-full object-contain object-center"/>
+      </div>
+      <div className="p-4 flex flex-col flex-1">
+        <h3 className="text-lg font-semibold text-gray-900 line-clamp-2 h-14 overflow-hidden">{product.title}</h3>
+          <p className="text-sm font-medium text-blue-900 mt-2">{product.category} $</p> 
+        <p className="text-sm font-medium text-blue-900 mt-2">{product.price} $</p> 
+
+        <button className="mt-4 bg-orange-500 text-white py-2 px-4 rounded hover:bg-orange-600 transition-colors duration-200">Details</button>
+      </div>
     </div>
   );
 }
