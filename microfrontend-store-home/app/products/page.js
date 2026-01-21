fcf272039eac2c4eb167ef118af3b44b0e6bbf6f
frostdead43 <frostdead@hotmail.com>
2026-01-21 18:47:32 +0300

    searchbar removed

--- microfrontend-store-home/app/products/page.js
+++ microfrontend-store-home/app/products/page.js
@@ -1,5 +1,4 @@
 import CardItem from "@/components/ProductCard";
-import Searchbar from "@/components/Searchbar";
 import { fetchProducts } from "@/lib/fetchers";
 import Link from "next/link";
 
@@ -7,7 +6,6 @@ export default async function Home() {
   const products = await fetchProducts();
   return (
     <div>
-      <Searchbar/>
   <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 mb mx-16 my-20">
     {products.map(x => (
       <Link key={x.id} href={`/products/${x.id}`}>
